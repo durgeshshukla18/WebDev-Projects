6440535493d6f60662b6a16ba924cc6fe70c28f7
Durgesh Shukla <thegdshukla@gmail.com>
2025-03-14 00:31:40 +0530

    Adding Task-crew

---
+++ Task-crew/src/df.js
@@ -0,0 +1,33 @@
+toDo = [
+    {
+        "title": "Buy groceries",
+        "status": "pending",
+        "prioirity": 2,
+    },
+    {
+        "title": "Finish project",
+        "status": "pending",
+        "prioirity": 1,
+    },
+    {
+        "title": "Clean room",
+        "status": "done",
+        "prioirity": 3,
+    }
+];
+
+// Sort the toDo array by priority in ascending order
+toDo.sort((a, b) => - a.prioirity + b.prioirity);
+
+
+function printToDo(toDo) {
+    
+}
+toDo.forEach((item) => {
+
+});
+console.log(toDo);
+
+document.onload = function() {
+    
+}
